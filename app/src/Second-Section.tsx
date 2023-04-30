0197a50dbc8ffb92beff69a33dfe8a4988d21065
joseph williams <wj882244@gmail.com>
2023-04-30 14:26:31 +0100

    update

--- app/src/Second-Section.tsx
+++ app/src/Second-Section.tsx
@@ -1,4 +1,8 @@
-
+import reacti from './Image/React.png'
+import tailwindi from './Image/tailwind.png'
+import cssi from './Image/css.png'
+import javascripti from './Image/javascript.png'
+import typescripti from "./Image/typescript.png";
 export default function SecondSection() {
     return (
       <section className=" bg-[#0b0c17] pl-10 pr-10 pb-20">
@@ -6,25 +10,25 @@ export default function SecondSection() {
           My Technical Skills :
         </h2>
         <aside className="w-full flex flex-wrap mt-6 justify-center gap-4">
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             Html
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             CSS
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             JavaScript
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             React Js
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             Tailwind
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             TypeScript
           </div>
-          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-1/4 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
+          <div className="shadow-md pl-3 pr-3 shadow-[#77b02e] w-2/5 pt-5 pb-5 bg-[#252535] text-center text-white font-sans font-medium">
             Git
           </div>
         </aside>
