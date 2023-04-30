9b1dfffd1a27a70cbd8116a5ed39c5ae13cadd6b
joseph williams <wj882244@gmail.com>
2023-04-30 19:36:15 +0100

    Update

--- app/src/FourthSection.tsx
+++ app/src/FourthSection.tsx
@@ -1,7 +1,7 @@
 export default function FourthSection() {
   return (
     <section className=" bg-[#0b0c17] pl-10 pr-10">
-      <h2 className="text-white font-bold text-2xl text-center pb-3">
+      <h2 className="text-white font-bold text-2xl  pb-3">
         About Me {""}{" "}
         <i className="fa-solid fa-arrow-down-short-wide text-FirstSHeader text-2xl"></i>
       </h2>
