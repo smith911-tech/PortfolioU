31e3c6433e04e6740ccab07bb43bb1e5cc10a704
joseph williams <wj882244@gmail.com>
2023-04-30 11:52:55 +0100

    update

--- app/src/First-Section.tsx
+++ app/src/First-Section.tsx
@@ -1,5 +1,5 @@
 import codeimg from "./Image/Computer.png";
-import LineOne from "./Image/LIne-1fS.png"
+import LineOne from "./Image/LIne-1fS.png";
 export default function FirstSection() {
   return (
     <div className=" bg-gradient-to-r from-FirstSL1 to-FirstSL2 pt-5 pl-10 pr-10 pb-5">
@@ -10,9 +10,8 @@ export default function FirstSection() {
           Developer
         </h1>
       </header>
-      <main className="relative">
-        <div className="w-full flex justify-center gap-8">
-          <span className=" w-2/5">
+        <div className="w-full flex justify-center gap-8 relative">
+          <span className=" w-2/5 z-10">
             <h1 className="text-white font-sans font-black text-3xl mt-6 mb-4">
               Welcome To Williams Joseph Portfolio
             </h1>
@@ -20,10 +19,9 @@ export default function FirstSection() {
               I will be Showing Off my stacks and project that i've Worked On
             </p>
           </span>
-          <img src={codeimg} alt="" className="ml-2 w-2/5" />
+          <img src={codeimg} alt="" className="ml-2 w-2/5 z-10" />
         </div>
-        <img src={LineOne} alt="" className="absolute top-3 w-2/3 z--10"/>
-      </main>
+        <img src={LineOne} alt="" className="fixed top-0  w-2/3 left-full" />
     </div>
   );
 }
