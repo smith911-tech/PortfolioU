f3b9a07967abb78de2f5b3bd811f1379df9bffe6
joseph williams <wj882244@gmail.com>
2023-05-07 12:01:46 +0100

    updated

--- app/src/FirthSection.tsx
+++ app/src/FirthSection.tsx
@@ -16,7 +16,7 @@ export default function FirthSection() {
           Contact Me {""}
           <i className="fa-solid fa-address-book text-FirstSHeader text-2xl"></i>
         </h2>
-        <div className="pl-10 pr-10 bg-[#20241c] flex gap-3 justify-center pt-3 pb-3">
+        <div className="pl-10 pr-10 flex gap-3 justify-center pt-3 pb-3">
           <a href="https://facebook.com/williams.joseph.56863">
             <img
               src="https://img.shields.io/badge/Facebook-%231877F2.svg?logo=Facebook&logoColor=white"
@@ -43,13 +43,13 @@ export default function FirthSection() {
           </a>
           <a href="https://wa.link/gwermi#">
             <img
-              src="https://img.shields.io/badge/Whatsapp-%10b743.svg?logo=Whatsapp&logoColor=white"
+              src="https://img.shields.io/badge/Whatsapp-%28D146.svg?logo=Whatsapp&logoColor=white"
               alt=""
             />
           </a>
           <a href="malito:JosephPay911@gmail.com">
             <img
-              src="https://img.shields.io/badge/Gmail-%10b743.svg?logo=Gmail&logoColor=white"
+              src="https://img.shields.io/badge/Gmail-%23E34F26.svg?logo=Gmail&logoColor=white"
               alt=""
             />
           </a>
