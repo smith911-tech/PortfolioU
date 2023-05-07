a32453a2eacd6a6181a2448e660b1dbaf9305052
joseph williams <wj882244@gmail.com>
2023-05-07 11:44:44 +0100

    updated

--- app/src/FirthSection.tsx
+++ app/src/FirthSection.tsx
@@ -22,17 +22,20 @@ export default function FirthSection() {
           <i className="fa-solid fa-address-book text-FirstSHeader text-2xl"></i>
         </h2>
         <div className="pl-10 pr-10 bg-[#20241c] flex gap-3 justify-center pt-3 pb-3">
-          <a href="https://facebook.com/williams.joseph.56863">
-            <img
-              src="https://img.shields.io/badge/Facebook-%231877F2.svg?logo=Facebook&logoColor=white"
-              alt=""
-            />
+          <a href="https://web.facebook.com/williams.joseph.56863">
+            <img src={facebook} alt="" />
           </a>
-          <a href="https://instagram.com/therealjoseph001">
-            <img
-              src="https://img.shields.io/badge/Instagram-%23E4405F.svg?logo=Instagram&logoColor=white"
-              alt=""
-            />
+          <a href="https://www.linkedin.com/in/joseph-williams-1a7533243/">
+            <img src={linkedin} alt="" />
+          </a>
+          <a href="https://wa.link/gwermi#">
+            <img src={whatsapp} alt="" />
+          </a>
+          <a href="https://github.com/smith911-tech">
+            <img src={github} alt="" />
+          </a>
+          <a href="https://twitter.com/TheRealJoseph01">
+            <img src={twitter} alt="" />
           </a>
         </div>
       </section>
