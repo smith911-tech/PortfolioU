75ac5ffbf0d8c23eeb7bbeed465b3efdbeae8d06
Williams Joseph <wj882244@gmail.com>
2023-10-23 23:37:21 +0100

    updating portfo9lio

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,9 @@
 import './App.css'
-import FirstSection from './First-Section'
-import FirthSection from './FirthSection';
-import FourthSection from './FourthSection';
-import SecondSection from './Second-Section'
-import ThirdSection from './Third-section';
-
 function App() {
 
   return (
     <main className='select-none'>
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <FourthSection />
-      <FirthSection />
+
     </main>
   );
 }
