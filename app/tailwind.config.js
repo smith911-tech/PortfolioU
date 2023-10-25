25ebeb61b9deda814668b1ec3b8ea6a0ecf4cffb
Williams Joseph <wj882244@gmail.com>
2023-10-26 00:44:53 +0100

    update

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -13,6 +13,11 @@ export default {
       'lg1024': '1024px',
       'lg1200': '1200px'
     },
+      boxShadow: {
+      'custom': '0px 2px 8px 0px rgba(2, 4, 45, 0.10)',
+      'box': '0px 5px 10px 1.25px rgba(215, 215, 215, 0.25)',
+      'makeReqModal': '0px 24px 43px 0px rgba(76, 76, 76, 0.07)'
+      },
     },
   },
   plugins: [],
