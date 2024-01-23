cfb9bc43b95195b2d541726bffee4cf9548fb145
Williams Joseph <wj882244@gmail.com>
2024-01-23 15:05:57 +0100

    update

--- app/src/FourthSection.tsx
+++ app/src/FourthSection.tsx
@@ -3,6 +3,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import bossky from "./assets/bossky.mp4";
 import { GiLaptop } from "react-icons/gi";
 import { LiaLaptopCodeSolid } from "react-icons/lia";
+import FriendVerse from './assets/FriendVerse.mp4';
 export default function FourthSection() {
   return (
     <main className="px-5 py-4 sm634:py-5 sm634:px-8 mb-12">
@@ -18,7 +19,7 @@ export default function FourthSection() {
           <video autoPlay muted loop className="w-full h-64 object-contain">
             <source src={bossky} type="video/mp4" />
           </video>
-          <div className=" px-4 pb-2 -mt-2">
+          <div className=" px-4 pb-2 mt-2">
             <p className="text-white font-Belanosima ">Boossky</p>
             <p className="text-[#ffffffb7] text-[12px] mt-2">
               This website offers a range of services including hair salon, skin
@@ -50,18 +51,18 @@ export default function FourthSection() {
         </section>
         <section className=" w-full min-[500px]:w-[450px] bg-black  rounded-xl shadow-box ">
           <video autoPlay muted loop className="w-full h-64 object-contain">
-            <source src={bossky} type="video/mp4" />
+            <source src={FriendVerse} type="video/mp4" />
           </video>
-          <div className=" px-4 pb-2 -mt-2">
-            <p className="text-white font-Belanosima ">Boossky</p>
+          <div className=" px-4 pb-2 mt-2">
+            <p className="text-white font-Belanosima ">FriendVerse</p>
             <p className="text-[#ffffffb7] text-[12px] mt-2">
-              This website offers a range of services including hair salon, skin
-              care, massage, and more in kenya. this is a collaborative project.
+              This social media web app allows you to post, share, comment, and
+              like posts and comments, users can also verify there account.
             </p>
             <div className="flex mt-6 justify-between">
               <a
                 target="blank"
-                href="https://bookssy.vercel.app/"
+                href="https://friend-verse.vercel.app/"
                 className="text-[white] flex gap-2 border p-2 rounded-md shadow-box"
               >
                 <p className="text-[12px] font-Belanosima font-semibold">
@@ -71,7 +72,7 @@ export default function FourthSection() {
               </a>
               <a
                 target="blank"
-                href="https://github.com/smith911-tech/bookssy"
+                href="https://github.com/Friend-Verse"
                 className="text-[white] flex gap-2 border p-2 rounded-md shadow-box"
               >
                 <p className="text-[12px] font-Belanosima font-semibold">
